5c6f4c1c15b97c3836d2a55f64faa59fa76b65cb
RafaelReyesCarmona <78477791+RafaelReyesCarmona@users.noreply.github.com>
2021-10-23 19:59:26 +0200

    V1.0

---
+++ src/UTMConversion.h
@@ -0,0 +1,69 @@
+/*
+UTMConsersion.h - Library to convert in UTM coordenates. Headers file.
+UTMConsersion v1.0
+
+Copyright © 2019-2021 Francisco Rafael Reyes Carmona.
+All rights reserved.
+
+rafael.reyes.carmona@gmail.com
+
+  This file is part of UTMConsersion.
+
+  UTMConsersion is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  UTMConsersion is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with UTMConsersion.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#if ARDUINO >= 100
+	#include "Arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+#ifndef UTMConversion_h
+#define UTMConversion_h
+
+class GPS_UTM {
+	private:
+		int _h;
+		char _letter;
+		long _x;
+		long _y;
+
+	public:
+    GPS_UTM(){
+			_h = 0;
+			_letter = 'Z';
+			_x = 0L;
+			_y = 0L;
+		};
+
+	void UTM(double, double);
+
+	int zone(){
+		return _h;
+	};
+
+	char band(){
+		return _letter;
+	};
+
+	long X(){
+		return _x;
+	};
+
+	long Y(){
+		return _y;
+	};
+};
+
+#endif

799455e08695216b9aa6f2de95b8a042902ed60f
RafaelReyesCarmona <78477791+RafaelReyesCarmona@users.noreply.github.com>
2022-01-09 23:32:55 +0100

    Update

--- examples/UTMConversion_simple/UTMConversion_simple.ino
+++ examples/UTMConversion_simple/UTMConversion_simple.ino
@@ -1,5 +1,5 @@
 /*
-UTMConsersion_example.cpp - Example to use this library to convert in UTM coordenates.
+UTMConsersion_simple.cpp - Example to use this library to convert in UTM coordenates.
 UTMConsersion v1.1
 
 Copyright © 2019-2021 Francisco Rafael Reyes Carmona.
@@ -25,28 +25,19 @@ rafael.reyes.carmona@gmail.com
 
 #include <UTMConversion.h>
 
-float flat[] = {37.8959210,40.033223,40.774997,66.077490,33.406994,-11.551785,7.333418,-41.266792,-21.643807,-37.463226};
-float flon[] = {-4.7478210,-2.567744,15.582325,-45.853359,-84.482198,-45.151016,-69.030662,-67.625977,18.059812,146.629184};
+float flat = 37.8959210;
+float flon = -4.7478210;
 
 GPS_UTM utm;
 
 void setup() {
-    char utmstr[] = "30S 123456 1234567";
+  char utmstr[] = "30S 123456 1234567";
 
-    Serial.begin(9600);  
+  Serial.begin(9600);  
 
-    for(int i = 0; i < 10;i++){
-        char line[50];
-        char strlat[12];
-        char strlon[12];
-        dtostrf(flat[i],8,6,strlat);
-        dtostrf(flon[i],8,6,strlon);
-        sprintf(line, "Coordenates %s, %s -> UTM: ", strlat, strlon);
-        Serial.print(line);
-        utm.UTM(flat[i], flon[i]);
-        sprintf(utmstr, "%02d%c %ld %ld", utm.zone(), utm.band(), utm.X(), utm.Y());
-        Serial.println(utmstr);
-    }
+  utm.UTM(flat, flon);
+  sprintf(utmstr, "%02d%c %ld %ld", utm.zone(), utm.band(), utm.X(), utm.Y());
+  Serial.println(utmstr);
 }
 
 void loop() {
